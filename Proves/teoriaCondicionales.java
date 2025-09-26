11a70e7ecd40ca99930236baf4bbed4ed18a48d6
mgundin7 <166124365+mgundin7@users.noreply.github.com>
2025-09-26 11:00:31 +0200

    Add files via upload

---
+++ Proves/teoriaCondicionales.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class teoriaCondicionales {
+    public static void main(String[] args) throws Exception {
+        //Programa que te dice si has aprobado
+
+        Scanner sc = new Scanner(System.in);
+        double nota;
+        System.out.println("Que nota has sacado? ");
+
+        nota = sc.nextDouble();
+
+        //Condicion, si la nota es mayor que 5 esta aprobado, sino esta suspendido.
+        if (nota >= 0) {
+            if (nota<5) {
+                System.out.println("Estas suspendido");
+            }
+            else if (nota >= 5 && nota<7) {
+                System.out.println("Estas aprobado con un suficiente");
+            }
+            else if (nota >= 7 && nota<9) {
+                System.out.println("Estas aprobado con un notable");
+            }
+            else if (nota >= 9 && nota <= 10) {
+                System.out.println("Estas aprobado con un excelente");
+            }
+        }
+        else{
+            System.out.println("el valor de tu nota no es valido, vuelva a insertarlo porfavor");
+        }
+    }   
+}
