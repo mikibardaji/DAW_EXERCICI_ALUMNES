316f44bfbcf4d4a28435f98ea154de1c6cdafffe
Jeremy610x <94995811+Jeremy610x@users.noreply.github.com>
2025-09-26 11:06:36 +0200

    JeremyPanchana

--- Proves/TeoriaCondicionales.java
+++ Proves/TeoriaCondicionales.java
@@ -2,23 +2,39 @@ import java.util.Scanner;
 
 public class TeoriaCondicionales {
     public static void main(String[] args) throws Exception {
-        
-        Scanner sc = new Scanner(System.in);
-        double nota;
-        System.out.println("Pedir nota ?");
-        nota = sc.nextDouble();
+       
+        Scanner sc = new Scanner (System.in);
 
-        if (nota<5){
-            System.out.println("Has suspes.");
-        }
-        else if (nota>=5 && nota<7){
-            System.out.println("Has aprovaf.");
-        }
-         else if (nota>=7 && nota<9){
-            System.out.println("Has sacado un notable.");
-        }
-         else if (nota>=9 && nota <=10){
-            System.out.println("Has sacado un excelentge.");
-        }
+        
+            System.out.println("Escribe la nota : ");
+            double nota = sc.nextDouble();
+            if (nota<5)
+                    {
+                    
+                          System.out.println("Has suspendido");
+                    
+                    
+                    }
+                    
+                    else if (nota>=5 && nota<7)
+                    
+                    {
+                        System.out.println("Aprobado");
+
+                    }
+                    else if(nota>=7 && nota<9){
+
+                        System.out.println("Notable");
+                    }
+                    else if (nota>=9 && nota<=10){
+
+                        System.out.println("Excelent");
+                    }
+           
+       
+            
+
+
+
     }
 }
