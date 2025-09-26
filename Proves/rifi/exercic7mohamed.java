d7bc9f029afdaaff953f58ac9545a1a1d7e37121
Mohamed1875575 <44614604+Mohamed1875575@users.noreply.github.com>
2025-09-26 13:47:46 +0200

    Add files via upload

---
+++ Proves/rifi/exercic7mohamed.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercic7mohamed {
+public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    int dato1, dato2, dato3;
+    System.out.println("pone dato1:");
+    dato1 = sc.nextInt();
+    System.out.println("pone dato 2");
+    dato2 = sc.nextInt();
+    System.out.println("pone dato 3");
+    dato3 = sc.nextInt();
+
+    if (dato1>=dato2 && dato1>=dato3){
+        System.out.println("dato elgrande es:" + dato1);
+    }
+    else if (dato2>=dato1 && dato2>=dato3){
+        System.out.println("dato elgrande es:" + dato2);
+    }
+    else if (dato3>=dato1 && dato3>=dato2){
+        System.out.println("dato elgrande es:" + dato3);
+    }
+
+}
+}
