e6d6f0e1fb64fe03acab9d832103656c057ed8e3
44danif <danielf.m3007@gmail.com>
2025-09-26 12:52:22 +0200

    Add files via upload

---
+++ Proves/Figuereo/FiguereoDanielEx3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FiguereoDanielEx3 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    //Pedir valor 1, valor 2
+    int valor1, valor2;
+    System.out.println("Introduce 1 valor: ");
+    valor1 = sc.nextInt();
+    System.out.println("Introduce un segundo valor: ");
+    valor2 = sc.nextInt();
+    //Si valor 1 > valor 2
+    //Mostrar valor 1 es mayor
+    if (valor1>valor2)
+    {
+        System.out.println("El numero mas grande es: " + valor1);
+    }
+
+    //Sino mostrar valor 2 es mayor
+      else if (valor1<valor2)
+    {
+        System.out.println("El valor 2 es mayor");
+    }
+    //Sino mostrar son iguales
+    else
+    {
+        System.out.println("Los dos numeros son iguales");
+    }
+}
+}
