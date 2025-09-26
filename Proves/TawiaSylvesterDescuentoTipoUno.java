cc826f9a0a2b8f04690fc5a334928cc84f7e9c09
Sylvester kwadwo tawia <savestine@gmail.com>
2025-09-26 11:00:55 +0200

    Update and rename DescuentoTipoUno.java to TawiaSylvesterDescuentoTipoUno.java

---
+++ Proves/TawiaSylvesterDescuentoTipoUno.java
@@ -0,0 +1,22 @@
+public class DescuentoTipoUno {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el precio original del producto:");
+        double precioOriginal = sc.nextDouble();
+
+        System.out.println("Introduce el porcentaje de descuento:");
+        double descuento = sc.nextDouble();
+        
+        double precioConDescuento = precioOriginal - (precioOriginal * (descuento / 100));
+
+       
+        System.out.println("El precio original es: " + precioOriginal);
+        System.out.println("El descuento es: " + descuento + "%");
+        System.out.println("El precio con descuento aplicado es: " + precioConDescuento);
+
+        sc.close();
+    }
+
+}
+
