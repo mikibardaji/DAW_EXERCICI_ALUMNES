85ab1cf3b369fe646003eb7953c0bec838bd5ef0
Goyo <163832659+Goyoisinter@users.noreply.github.com>
2025-09-30 10:32:58 +0200

    Update Ex10Condicionales.java

--- Proves/Goyo/Ex10Condicionales.java
+++ Proves/Goyo/Ex10Condicionales.java
@@ -1,36 +1,30 @@
 import java.util.Scanner;
 
-public class Salario {
+public class horasTrabajadas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Introduce el numero de horas trabajadas: ");
+        final int TARIFA = 15;
+        double dineroBase = 0;
+        double dineroExtra = 0;
+
+        System.out.print("Introduce el numero de horas trabajadas:");
         double horas = sc.nextDouble();
 
-        double salarioBruto;
-        if (horas <= 130) {
-            salarioBruto = horas * 15.0;
-        } else {
-            salarioBruto = (130 * 15.0) + ((horas - 130) * 22.5);
-        }
-
-        double impuestos;
-        if (salarioBruto <= 500) {
-            impuestos = 0;
-        } else if (salarioBruto <= 900) {
-            impuestos = (salarioBruto - 500) * 0.25;
-        } else {
-            impuestos = (400 * 0.25) + ((salarioBruto - 900) * 0.45);
-        }
-
-        double salarioNeto = salarioBruto - impuestos;
-
-        System.out.printf("Salari brut: " + salarioBruto  ); 
-        System.out.printf("Taxes:" + impuestos  ); 
-        System.out.printf("Salari net: " + salarioNeto);
-
-        sc.close();
-
+        if (horas >= 130) 
+            {
+             double extra = horas - 130;
+             dineroExtra = extra*1.5*TARIFA;
+             dineroBase = 130*TARIFA;
+             double sueldo = dineroExtra + dineroBase;
+             System.out.println("Tu sueldo final es " + sueldo + "euros");   
+            }
+        else
+            {
+             double total = dineroBase+horas*TARIFA;
+             System.out.println("Tu sueldo final es " + total + "euros");   
+            }
+            
     }
 
 }
