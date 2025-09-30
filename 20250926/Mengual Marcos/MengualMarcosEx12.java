01303f54c8458de328926f7200260fc6677aa998
marcosmengual8 <marcosmengual8@gmail.com>
2025-09-30 08:40:46 +0200

    MengualMarcosEx12.java

--- 20250926/Mengual Marcos/MengualMarcosEx12.java
+++ 20250926/Mengual Marcos/MengualMarcosEx12.java
@@ -1,3 +1,39 @@
+    // Pedir color del semáforo al usuario
+    // Si color == "V"
+    //     Mostrar "Puedes pasar"
+    // Si color == "N"
+    //     Mostrar "Debes esperar"
+    // Si color == "R"
+    //     Mostrar "Detente"
+
+
+
+import java.util.Scanner;
+
 public class MengualMarcosEx12 {
+    public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    char colorsemaforo;
+    System.out.println("Introduce el color del semaforo (R-Rojo, N-Naranja, V-verde): ");
+    colorsemaforo= sc.next().charAt(0);
+
+    if (colorsemaforo == 'V') {
+
+            System.out.println("Puedes pasar");
+
+        } else if (colorsemaforo == 'N') {
+
+            System.out.println("Debes esperar");
+
+        } else if (colorsemaforo == 'R') {
+
+            System.out.println("Detente");
+        } else {
+            
+            System.out.println("Valor incorrecto, introduce V, N o R");
+        }
+
 
 }
+}
+
