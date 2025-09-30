457e5ccd891fbd691b947524dc2100ccf8485fed
jordirequena <jordi.requena.ga@gmail.com>
2025-09-30 09:53:16 +0200

    Add files via upload

---
+++ 20250926/RequenaJordi/Vueltadecambio.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Vueltadecambio {
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        System.out.print("Escriu un preu: ");
+        double preu = sc.nextDouble();
+
+        System.out.print("Quina quantitat pagues: ");
+        double quantitat = sc.nextDouble();
+
+        if (preu > quantitat){
+            double resta = preu - quantitat;
+            System.out.println("Et sobran " + resta);
+
+        } else if (quantitat>preu) {
+            double resta = quantitat - preu;
+            System.out.println("Et queden " + resta);
+}
+}
+
+}
