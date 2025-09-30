fa039b6da58806a9e1e0cfd487cbda868b61c9f6
Marck Montaño <marck.dplan@gmail.com>
2025-09-30 11:27:44 +0200

    Add files via upload

--- 20250926/Antony/Ex10.java
+++ 20250926/Antony/Ex10.java
@@ -4,11 +4,13 @@ public class Ex10 {
 public static void main(String[] args) {
     Scanner lector = new Scanner(System.in);
     final double TARIFA = 15.00;
-    double dinerobase, dineroextra=0;
     final double extra = 1.5;
-    double sueldo, horasextra;
+    final double sin_impuestos = 1000;
+    final double porcentaje_impuestos = 25%;
+    double dinerobase, dineroextra= 0;
+    double sueldo, horasextra, Horas;
     System.out.println("Horas de trabajo: ");
-    int Horas = lector.nextInt();
+    Horas = lector.nextDouble();
     if (Horas>=130) 
     {
         horasextra = Horas - 130;
@@ -20,8 +22,16 @@ public static void main(String[] args) {
         dinerobase = Horas * TARIFA;
     }
          sueldo = dinerobase + dineroextra;
+        System.out.println("Vas a cobrar normal "+ dinerobase + " euros ");
         System.out.println("vas a combrar con horas extras " + sueldo + " euros");
-
-
+        System.out.println("Vas a cobrar sin impuestos " + sueldo_final+ "euros");
+    Double impuestos = 0 , sueldo_superior;
+    if (sueldo > sin_impuestos)
+        {
+            sueldo_superior = sueldo - sin_impuestos;
+            impuestos = sueldo * porcentaje_impuestos;
+        }
+        sueldo = sueldo - impuestos;
+        System.out.println("Impuestos");
     }
 }
