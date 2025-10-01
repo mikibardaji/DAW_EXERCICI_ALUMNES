b27dce914dbd0736f90e01342498c7641491aab7
Harpreet Kaur <150058584+haka2191@users.noreply.github.com>
2025-10-01 23:07:52 +0200

    Update clashRoyale.java

--- 20250930/Singh/clashRoyale.java
+++ 20250930/Singh/clashRoyale.java
@@ -3,7 +3,31 @@ import java.util.Scanner;
 public class clashRoyale {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-
+        /*Inici
+        Preguntar: "Quantes copes tens?"
+        Llegir el número de copes.
+        Decidir opcions segons el número de copes:
+        Si copes < 2000 → Opcions:
+        (m) Mago Eléctrico
+        (e) Esbirro Mega
+        Si 2000 ≤ copes < 3000 → Opcions:
+        (c) Caballero
+        (b) Bandida
+        Si copes ≥ 3000 → Opcions:
+        (e) Ejército de esqueletos
+        (g) Gigante
+        Mostrar per pantalla les dues opcions disponibles.
+        Preguntar: "Escriu la lletra de la carta que vols:"
+        Llegir la lletra introduïda per l’usuari (acceptar majúscules i minúscules).
+        Comparar la lletra amb les opcions disponibles segons el rang de copes:
+        Si coincideix amb una de les opcions → guardar el nom complet de la carta.
+        Si no coincideix → marcar com a "opció no vàlida".
+        Mostrar el resultat:
+        Si la lletra era vàlida → "Has escollit XXXX".
+        Si no era vàlida → "Opció no vàlida".
+        Fi del programa.
+        */
+        
         // Demanar al usuari les copes
         System.out.println("Quantes copes tens?");
         int copes = scanner.nextInt();
@@ -60,3 +84,4 @@ public class clashRoyale {
         }
     }
 }
+
