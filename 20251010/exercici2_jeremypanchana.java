63525573eb2f7dee01109802b9b694fd9d5756a6
Jeremy610x <94995811+Jeremy610x@users.noreply.github.com>
2025-10-13 16:37:44 +0200

    Add files via upload

---
+++ 20251010/exercici2_jeremypanchana.java
@@ -0,0 +1,27 @@
+public class exercici2_jeremypanchana {
+
+
+
+    /*
+     * Realiza un programa que muestre los números pares comprendidos
+     * entre el 1 y el 200. Para ello
+    utiliza un contador y suma de 2 en 2.
+     * 
+     */
+
+    public static void main(String[] args) {
+        
+        int contador =2;
+
+        while(contador<=200){
+
+            System.out.println(contador);
+
+            contador = contador +=2;
+
+
+        }
+
+
+    }
+}
