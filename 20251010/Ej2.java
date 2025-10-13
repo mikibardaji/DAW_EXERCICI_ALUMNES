d1c6889193eea2a93af290f8fc0f52b5a3c54bd0
RaulCasass <149099769+RaulCasass@users.noreply.github.com>
2025-10-13 22:03:14 +0200

    CasasRaul_Ejercicio2

---
+++ 20251010/Ej2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej2;
+
+/**
+ *
+ * @author rasu2
+ */
+public class Ej2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contador = 0;
+        
+        do{
+            contador = contador + 2;
+            System.out.println(contador);
+        }while(contador != 200);
+    }
+    
+}
