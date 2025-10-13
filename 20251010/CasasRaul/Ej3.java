529077e1fd73b3bb1cb7bfcbebfe63a7a013dcaf
RaulCasass <149099769+RaulCasass@users.noreply.github.com>
2025-10-13 22:04:05 +0200

    Add files via upload

---
+++ 20251010/CasasRaul/Ej3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej3;
+
+/**
+ *
+ * @author rasu2
+ */
+public class Ej3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contador = 1;
+
+        do  {
+            if (contador % 2 == 0) { 
+                System.out.println(contador);
+            }
+            contador++; 
+        }while(contador != 202);
+    }
+    
+}
