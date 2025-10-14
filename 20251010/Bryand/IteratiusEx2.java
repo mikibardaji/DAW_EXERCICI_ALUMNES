5f90d462129840a58ed2cba9566098a631219039
bryand <48119387+bryanddavid@users.noreply.github.com>
2025-10-14 08:50:11 +0200

    Add files via upload

---
+++ 20251010/Bryand/IteratiusEx2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class IteratiusEx2 {
+    public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+
+        int contador =2;
+        while (contador <= 200) {
+                System.out.println(contador);
+                contador+=2 ;
+                
+        }
+    }
+}
