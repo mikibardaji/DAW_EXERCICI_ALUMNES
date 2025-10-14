e223533d8e08a7a11e3df79568f766587dede4fd
jordirequena <jordi.requena.ga@gmail.com>
2025-10-14 10:02:57 +0200

    Add files via upload

---
+++ 20251003/E1_federats.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class E1_federats {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Introdueix l'any de naixement del federat: ");
+        int anyNaixement = entrada.nextInt();
+
+        if (anyNaixement < 1924) {
+            System.out.println(" Aquest any no és vàlid per a jugar a categories actuals.");
+        } else {
+            String categoria;
+
+            if (anyNaixement > 2013) {
+                categoria = "No categoria";
+            } else if (anyNaixement >= 2011 && anyNaixement <= 2012) {
+                categoria = "S14";
+            } else if (anyNaixement >= 2009 && anyNaixement <= 2010) {
+                categoria = "S16";
+            } else if (anyNaixement >= 2007 && anyNaixement <= 2008) {
+                categoria = "S18";
+            } else if (anyNaixement >= 1991 && anyNaixement <= 2006) {
+                categoria = "Absoluta";
+            } else { // abans de 1991 però després o igual a 1924
+                categoria = "Veterans";
+            }
+
+            System.out.println(" El jugador estarà federat a la categoria " + categoria);
+        }
+
+    }
+}
