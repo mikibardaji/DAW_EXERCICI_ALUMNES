f02cf2e4911b2cb4fe152fde274e6d44a4ac1b9d
Marck Montaño <marck.dplan@gmail.com>
2025-10-15 10:35:06 +0200

    Delete 20251010/src directory

--- 20251010/src/Ex2.java
+++
@@ -1,7 +0,0 @@
-public class Ex2 {
-public static void main(String[] args) {
-    int contador;
-     for (contador=1; contador <=200; contador=contador+2)
-     { System.out.println(contador);}
-}
-}
--- 20251010/src/Ex3.java
+++
@@ -1,9 +0,0 @@
-public class Ex3 {
-public static void main(String[] args) throws InterruptedException {
-    int contador;
-    for (contador=1;contador<=200;contador++)
-    {
-        System.out.println(contador);
-    }
-}
-}
