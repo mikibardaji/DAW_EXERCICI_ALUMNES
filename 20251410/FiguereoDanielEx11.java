217c78023e2c9859f2f82b5ab78e218949889b74
44danif <danielf.m3007@gmail.com>
2025-10-16 23:01:57 +0200

    Add files via upload

---
+++ 20251410/FiguereoDanielEx11.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class FiguereoDanielEx11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        int numeroSecreto = rd.nextInt(100) + 1;
+        int intento = 0;
+        int contador = 0;
+
+        System.out.println("¡Adivina el número que estoy pensando (del 1 al 100)!");
+
+        do {
+            System.out.print("Escribe tu número: ");
+            intento = sc.nextInt();
+            contador++;
+
+            if (intento < numeroSecreto) {
+                System.out.println("El número secreto es MAYOR.");
+            } else if (intento > numeroSecreto) {
+                System.out.println("El número secreto es MENOR.");
+            } else {
+                System.out.println("¡Felicidades! Adivinaste el número en " + contador + " intentos.");
+            }
+
+        } while (intento != numeroSecreto);
+    }
+}
