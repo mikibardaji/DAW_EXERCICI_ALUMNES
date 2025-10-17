c8d29b0c60a1e6a6a98ee3228e668be9aa644acd
marcosmengual8 <marcosmengual8@gmail.com>
2025-10-17 11:11:17 +0200

    Add files via upload

---
+++ 20251410/intentaadivinar.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class intentaadivinar {
+
+    public static void main(String[] args) {
+        Random rd = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        int numeroSecreto = rd.nextInt(1, 101); 
+        int intento;
+        int contador = 0;
+
+        System.out.println("He pensado un número entre 1 y 100.");
+
+        do {
+            System.out.print("Introduce tu número: ");
+            intento = sc.nextInt();
+            contador++;
+
+            if (intento < numeroSecreto) {
+                System.out.println("Mi número es mayor.");
+            } else if (intento > numeroSecreto) {
+                System.out.println("Mi número es menor.");
+            } else {
+                System.out.println("Has adivinado el número en " + contador + " intentos.");
+            }
+
+        } while (intento != numeroSecreto && contador <=6);
+
+    }
+}
