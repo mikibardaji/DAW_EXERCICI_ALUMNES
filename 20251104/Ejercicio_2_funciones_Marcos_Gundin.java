074fc7f6528c6c39bfdd3c55d0957755d0966fad
mgundin7 <166124365+mgundin7@users.noreply.github.com>
2025-11-06 13:21:09 +0100

    Add files via upload

---
+++ 20251104/Ejercicio_2_funciones_Marcos_Gundin.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio_2_funciones_Marcos_Gundin {
+    public static void main(String[] args) throws Exception {
+     Scanner sc = new Scanner(System.in);
+        int edad;
+
+        System.out.print("Escribe tu edad: ");
+        edad = sc.nextInt();
+
+        if (esMayorEdad(edad)) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("No eres mayor de edad");
+        }
+    }
+
+  
+    static boolean esMayorEdad(int a) {
+        if (a >= 18) {
+            return true;
+        } else {
+            return false;
+        } 
+    }
+}
