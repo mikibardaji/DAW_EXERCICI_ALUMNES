befb713a9d83094ecf2ba984d4c9168ecdb03f28
Goyo <163832659+Goyoisinter@users.noreply.github.com>
2025-11-07 02:48:49 +0100

    Update ex4Funciones.java

--- 20251104/Heras Goyo/ex4Funciones.java
+++ 20251104/Heras Goyo/ex4Funciones.java
@@ -1,3 +1,32 @@
-public class ex4Funciones {
+import java.util.Scanner;
 
+public class ex4Funciones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce un número entero: ");
+        int numero = sc.nextInt();
+
+        int signo = dimeSigno(numero);
+
+        if (signo == -1) {
+            System.out.println("El número es negativo.");
+        } else if (signo == 0) {
+            System.out.println("El número es cero.");
+        } else {
+            System.out.println("El número es positivo.");
+        }
+
+        sc.close();
+    }
+
+    public static int dimeSigno(int a) {
+        if (a < 0) {
+            return -1;
+        } else if (a == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
