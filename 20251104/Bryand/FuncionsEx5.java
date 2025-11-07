74032c147e97f96833966b5a48a5c3376cd99819
bryand <48119387+bryanddavid@users.noreply.github.com>
2025-11-07 11:19:41 +0100

    Update FuncionsEx5.java

--- 20251104/Bryand/FuncionsEx5.java
+++ 20251104/Bryand/FuncionsEx5.java
@@ -1,11 +1,7 @@
 import java.util.Scanner;
 
 public class FuncionsEx5 {
-    public static double millas_km(int millas) {
-        return millas * 1.60934;
-    }
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Introduce la distancia en millas: ");
@@ -14,5 +10,10 @@ public class FuncionsEx5 {
         double kilometros = millas_km(millas);
 
         System.out.println(millas + " millas en kilometros son: " + kilometros + " km.");
+    } 
+    
+    public static double millas_km(int millas) {
+        return millas * 1.60934;
     }
 }
+
