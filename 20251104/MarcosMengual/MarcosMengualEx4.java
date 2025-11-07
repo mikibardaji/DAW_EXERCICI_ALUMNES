20b18f80da74ba9551cfdbd7375e261478af8333
marcosmengual8 <marcosmengual8@gmail.com>
2025-11-07 11:02:40 +0100

    Add files via upload

---
+++ 20251104/MarcosMengual/MarcosMengualEx4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MarcosMengualEx4 {
+public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+
+     System.out.print("Escribe un numero entero: ");
+        int num = sc.nextInt();
+
+        int signo = dimeSigno(num);
+
+        if (signo == -1) {
+            System.out.println("El numero es negativo.");
+        } else if (signo == 0) {
+            System.out.println("El numero es cero.");
+        } else {
+            System.out.println("El numero es positivo.");
+        }
+    }
+  
+       public static int dimeSigno(int a) {
+           if (a < 0) {
+            return -1;
+        } else if (a == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
