bbc414167769f9a6a73a290123c0d708b13bdefd
44danif <danielf.m3007@gmail.com>
2025-11-07 10:55:09 +0100

    Add files via upload

---
+++ 20251104/FiguereoDaniel/FiguereoDanielEx5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FiguereoDanielEx5 {
+ public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+     System.out.print("Escribe. tu distancia en millas: ");
+     int millas =  sc.nextInt();
+
+
+    double kilometros = millas_a_kilometros(millas);
+    System.out.println(millas + " millas equivalen a " + kilometros + " kilometros.");
+    }
+  
+       public static double millas_a_kilometros(int millas) {
+      
+         return  millas * 1.60934;
+    }
+}
