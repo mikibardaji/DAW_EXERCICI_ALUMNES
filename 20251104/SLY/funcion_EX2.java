c1647cd2a28ac6e15bbe3871d1da6ffe6a39a61c
Sylvester kwadwo tawia <savestine@gmail.com>
2025-11-07 10:55:01 +0100

    Update funcion_EX2.java

--- 20251104/SLY/funcion_EX2.java
+++ 20251104/SLY/funcion_EX2.java
@@ -4,27 +4,39 @@ import java.util.Scanner;
 public class funcion_EX2 {
     public static void main(String[] args) throws Exception {
 
-    int usuario = preguntarEdad();
-    boolean mayorDeEdad = esMayorDeEdadç(usuario);
+        int usuario = preguntarEdadValida();
+        boolean mayorDeEdad = esMayorDeEdadç(usuario);
+        mostrarResultado(mayorDeEdad);
 
     }
 
-    public static boolean esMayorDeEdadç(int age){
-        if (age >= 18){
-            System.out.println("You are an adult");
+    private static void mostrarResultado(boolean mayorDeEdad) {
+        if (mayorDeEdad){
+            System.out.println("you are an adult");
+
+        }else{
+            System.out.println("you are a minor");
+        }
+    }
+
+    public static boolean esMayorDeEdadç(int age) {
+        if (age >= 18) {
             return true;
         } else {
-            System.out.println("You are a minor");
             return false;
         }
-
-
     }
-    public static int preguntarEdad(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("how old are you?");
-        int user_age = sc.nextInt();
-        sc.close();
+    /**
+     * 
+     * @return edad positiva;
+     */
+    public static int preguntarEdadValida() {
+        int user_age;
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("how old are you? ");
+            user_age = sc.nextInt();
+        } while (user_age <= 0);
         return user_age;
     }
 
