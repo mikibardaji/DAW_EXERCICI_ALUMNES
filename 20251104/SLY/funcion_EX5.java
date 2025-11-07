eb85690ea72e47d58babee78a93e1724f56b014f
Sylvester kwadwo tawia <savestine@gmail.com>
2025-11-07 01:22:30 +0100

    Add files via upload

---
+++ 20251104/SLY/funcion_EX5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class funcion_EX5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce una distancia en millas: ");
+        int millas = sc.nextInt();
+
+        double kilometros = millas_a_kilometros(millas);
+
+        System.out.println(millas + " millas equivalen a " + kilometros + " kilómetros.");
+
+        sc.close();
+    }
+
+   
+    public static double millas_a_kilometros(int millas) {
+        return millas * 1.60934;
+    }
+}
