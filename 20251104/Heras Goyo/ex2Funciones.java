413c39348a779b0219be3c50d8280588e8f4638a
Goyo <163832659+Goyoisinter@users.noreply.github.com>
2025-11-07 02:49:03 +0100

    Update ex2Funciones.java

--- 20251104/Heras Goyo/ex2Funciones.java
+++ 20251104/Heras Goyo/ex2Funciones.java
@@ -1,3 +1,26 @@
-public class ex2Funciones {
+import java.util.Scanner;
 
+public class ex2Funciones {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce tu edad: ");
+        int edad = sc.nextInt();
+
+        if (esMayorEdad(edad)) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("No eres mayor de edad.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean esMayorEdad(int e) {
+        if (e >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
