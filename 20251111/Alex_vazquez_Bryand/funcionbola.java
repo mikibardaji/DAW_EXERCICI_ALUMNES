19a959cc165fcd43222a50d39a64834093d78b8a
alexvazoliv-afk <alexvaz.oliv@gmail.com>
2025-11-11 11:01:08 +0100

    Update funcionbola.java

--- 20251111/Alex_vazquez_Bryand/funcionbola.java
+++ 20251111/Alex_vazquez_Bryand/funcionbola.java
@@ -1,9 +1,19 @@
-import java.util.Random;
-
-public class funcionbola {
-    public static int bola(){
-        Random rd = new Random();
-        int numero_bola = rd.nextInt(0,37);
-        return numero_bola;
+public class funcionruleta2 {
+    public static void main(String[] args) {
+        pintarAsterisc(25);
     }
+    public static void pintarAsterisc(int numero){
+        final String AZUL = "\u001B[34m";
+        final String RESET = "\u001B[0m";
+        final String AMARILLO = "\u001B[33m";
+        final String ROJO = "\u001B[31m";
+
+        System.out.print("[");
+        for(int i = 0; i < numero; i++){
+            System.out.print(AZUL+"*" + RESET);
+        }    
+        System.out.println("]"+AMARILLO+ "--> " +ROJO+ numero + RESET);
+    }
+
 }
+    
