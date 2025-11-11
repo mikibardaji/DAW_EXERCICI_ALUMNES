4d4ff6396b48f1efe27f6f7c1e56c427ba2e0370
RaulCasass <149099769+RaulCasass@users.noreply.github.com>
2025-11-11 10:46:01 +0100

    Ruleta

--- 20251111/Ruleta.java
+++ 20251111/Ruleta.java
@@ -1,59 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ruleta;
+
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ *
+ * @author rca4813
+ */
 public class Ruleta {
-    public static void main(String[] args) throws Exception {
 
-        int num = 5;
-        pintaAsterisc(num);
-        System.out.println();
-
-        System.out.println("voy a imprimir un numero aleatorio entre 0 y 36");
-        System.out.println(bola());
-
-        int quentidad_apostar = QuantitatAposta(0);
-
-        System.out.println("Has apostado: " + quentidad_apostar + "quantidad.");
-
-    }
-    /*
-     * Funció pintaAsterisc: rep un número enter i imprimeix per
-     * pantalla tants asteriscs com indiqui el número rebut.
-     * Els asteriscs s'han de pintar amb linea (com es va demanar al examen)
-     * Al final dels asterisc pintar el numero.
+    /**
+     * @param args the command line arguments
      */
-
+    public static void main(String[] args) {
+        // TODO code application logic here
+       QuantitatAposta();
+       AfegirPunts(50);
+       pintaAsterisc(5);
+       tipusAposta();
+       bola();
+       apostaPunts(50, 10);
+       resultatJugada(2, 50, 10, 31);
+       
+    }
+    
+    public static int QuantitatAposta() {
+        Scanner sc = new Scanner(System.in);
+        int aposta;
+        
+        do{
+            System.out.print("Quina quantitat vols apostar? (0 per acabar): ");
+            aposta = sc.nextInt();
+            if(aposta<0){
+                System.out.println("No puedes poner un numero en negativo");
+            }
+        }while(aposta > 0 && aposta != 0);
+        return aposta;
+    }
+    
+    public static int AfegirPunts(int saldo){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introdueix quants punts vols afegir al saldo: ");
+        int afegir = sc.nextInt();
+        saldo = saldo + afegir;
+        return saldo;
+    }
+    
+    public static int bola() {
+        Random rand = new Random();
+        int resultat = rand.nextInt(1, 37);
+        System.out.println("La bola ha caigut al numero: " + resultat);
+        return resultat;
+    }
+    
+    public static int tipusAposta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escull el tipus d'aposta:");
+        System.out.println("1. PARELL");
+        System.out.println("2. SENAR");
+        System.out.println("3. Número concret (0-36)");
+        int opcio = sc.nextInt();
+        
+        if (opcio == 1) {
+            return 2;  
+        } else if (opcio == 2) {
+            return 1;  
+        } else {
+            System.out.print("Introdueix el número al qual vols apostar: ");
+            int numero = sc.nextInt();
+            return numero;
+        }
+    }
+    
+    public static int apostaPunts(int puntsTotal, int puntsAposta) {
+        if (puntsAposta <= 0 || puntsAposta > puntsTotal) {
+            System.out.println("Aposta no valida!");
+        }
+        return puntsAposta;
+    }
+    
+    public static int resultatJugada(int tipusAposta, int saldo, int puntsAposta, int bola) {
+        if (tipusAposta == 2) { 
+            if (bola != 0 && bola % 2 == 0) {
+                saldo = saldo + puntsAposta;
+                System.out.println("Has guanyat! +" + puntsAposta + " punts.");
+            } else {
+                saldo = saldo - puntsAposta;
+                System.out.println("Has perdut " + puntsAposta + " punts.");
+            }
+        } else if (tipusAposta == 1) { 
+            if (bola % 2 != 0) {
+                saldo = saldo + puntsAposta;
+                System.out.println("Has guanyat! +" + puntsAposta + " punts.");
+            } else {
+                saldo = saldo - puntsAposta;
+                System.out.println("Has perdut " + puntsAposta + " punts.");
+            }
+        } else { 
+            if (bola == tipusAposta) {
+                saldo = saldo + puntsAposta * 36;
+                System.out.println("Has encertat el número! +" + (puntsAposta * 36) + " punts.");
+            } else {
+                saldo = saldo - puntsAposta;
+                System.out.println("No has encertat el número.");
+            }
+        }
+        return saldo;
+    }
+    
     public static void pintaAsterisc(int numero) {
-
         for (int i = 0; i < numero; i++) {
             System.out.print("*");
         }
-        System.out.print(" = " + numero);
+        System.out.println(" " + numero);
     }
-
-    public static int bola() {
-        Random rand = new Random();
-        return rand.nextInt(0, 37);
     }
+    
 
-    public static int QuantitatAposta(int cantidad){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(" Introduce la cantidad a apostar: ");
-        cantidad = sc.nextInt();
-        return cantidad;
-
-    }
-
-    public static int resultatJugada (int tipusAposta,int saldo,int puntsAposta,int bola){
-        int guany = 0;
-        if (tipusAposta == bola){
-            guany = puntsAposta * 36;
-        } else {
-            guany = puntsAposta;
-        }if (tipusAposta == -1 || tipusAposta == -2){
-            guany = puntsAposta * 2;
-        }
-        return guany;
-    }
-
-}
