a5808471bde6ad4d44b7aec2876869564fe3a42f
bryand <48119387+bryanddavid@users.noreply.github.com>
2025-11-14 11:13:46 +0100

    Update Funcion_JuegodeCarta.java

--- 20251114/Bryand/Funcion_JuegodeCarta.java
+++ 20251114/Bryand/Funcion_JuegodeCarta.java
@@ -12,60 +12,56 @@ public class Funcion_JuegodeCarta {
     }
 
     public static int numero_carta_aleatori() {
-        int num = rd.nextInt(10) + 1;
-        if (num == 8) num = 10;
-        else if (num == 9) num = 11;
-        else if (num == 10) num = 12;
+        int num = rd.nextInt(1, 11);
+
+        if (num == 8) { 
+            num = 10;
+        }
+        if (num == 9) { 
+            num = 11;
+        }
+        if (num == 10) { 
+            num = 12;
+        }
         return num;
     }
 
     public static int pal_carta_aleatori() {
-        return rd.nextInt(4) + 1;
+        int palo = rd.nextInt(1,5);
+        return palo;
     }
 
     public static void imprimir_carta(int numero, int pal) {
-    String nomPal = "";
-    switch (pal) {
-        case 1:
+          String nomPal = "";
+        if (pal == 1) {
             nomPal = "OROS";
-            break;
-        case 2:
+        } else if (pal == 2) {
             nomPal = "BASTOS";
-            break;
-        case 3:
+        } else if (pal == 3) {
             nomPal = "ESPADAS";
-            break;
-        case 4:
+        } else if (pal == 4) {
             nomPal = "COPAS";
-            break;
-        default:
-            nomPal = "DESCONEGUT";
-    }
+        } else {
+            nomPal = "DESCONOCIDO";
+        }
 
-    String nomCarta;
-    switch (numero) {
-        case 10:
+        String nomCarta = "";
+        if (numero == 10) {
             nomCarta = "SOTA";
-            break;
-        case 11:
-            nomCarta = "CAVALL";
-            break;
-        case 12:
-            nomCarta = "REI";
-            break;
-        default:
+        } else if (numero == 11) {
+            nomCarta = "CABALLO";
+        } else if (numero == 12) {
+            nomCarta = "REY";
+        } else {
             nomCarta = String.valueOf(numero);
+        }
+
+        System.out.println("Carta: " + nomCarta + " de " + nomPal);
     }
-
-
-    System.out.println("Carta: " + nomCarta + " de " + nomPal);
-}
-
 
     public static double punts_carta(int numero) {
         if (numero >= 1 && numero <= 7) return numero;
-        else if (numero >= 10 && numero <= 12) return 0.5;
-        else return 0;
+        else return 0.5;
     }
 
     public static boolean banca_demana_carta(double puntsJugador, double puntsBanca) {
