1070d25d165e91696444a7b2c100d91d08b9f73d
bryand <48119387+bryanddavid@users.noreply.github.com>
2025-11-25 00:00:05 +0100

    20251125

--- 20251125/Bryand/TeoriaPOO/TeoriaPOO.java
+++ 20251125/Bryand/TeoriaPOO/TeoriaPOO.java
@@ -13,12 +13,13 @@ public class TeoriaPOO {
         System.out.println("Contenido actual: " + b.getContingut() + "ml");
 
         b.abrirBotella();
-        System.out.println("\nBotella abierta:");
+        System.out.println("Botella abierta:");
         System.out.println("Intento verter: " + b.verter() + "ml");
         System.out.println("Contenido actual: " + b.getContingut() + "ml");
 
-        System.out.println("\nIntento verter más de lo que queda:");
+        System.out.println("Intento verter más de lo que queda:");
         System.out.println("Intento verter 10ml: " + b.verter() + "ml");
         System.out.println("Contenido actual: " + b.getContingut() + "ml");
     }
 }
+
