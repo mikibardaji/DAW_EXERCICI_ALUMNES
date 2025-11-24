5537fa38c3b142b1e1d80886b4e6e2a2ec136ef5
MKBOB86 <150728291+MKBOB86@users.noreply.github.com>
2025-11-24 17:40:43 +0100

    20251125

---
+++ 20251125/MK/Botella.java
@@ -0,0 +1,47 @@
+package Objectes;
+
+public class Botella {
+    private int capacitat;      
+    private int contingut;      
+    private boolean tapada;
+
+    public Botella(int capacitat, int contingut, boolean tapada) {
+        this.capacitat = capacitat;
+        this.contingut = contingut;
+        this.tapada = tapada;
+    }
+
+
+    public int verter() {
+        return verter(5);
+    }
+
+    
+    public int verter(int quantitat) {
+    
+        if (tapada) return 0;
+
+    
+        if (contingut <= 0) return 0;
+
+    
+        int real = Math.min(quantitat, contingut);
+
+        contingut -= real;
+        return real;
+    }
+
+    public void tapar() {
+        tapada = true;
+    }
+
+    public void destapar() {
+        tapada = false;
+    }
+
+    public int getContingut() {
+        return contingut;
+    }
+}
+
+
