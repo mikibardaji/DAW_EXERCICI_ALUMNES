ce706f4f0a5acb2244ec6d23df66aac787e3be7e
bryand <48119387+bryanddavid@users.noreply.github.com>
2025-11-25 09:08:21 +0100

    20251125

--- 20251125/Bryand/Objetos/vaso.java
+++ 20251125/Bryand/Objetos/vaso.java
@@ -1,31 +1,30 @@
-package Objetos;
+package Objectes;
 
-public class vaso {
-
+public class Vaso {
     private int ml_liquido;
     private int ml_capacidad;
     private String material;
+    /**
+    int cm_alto;
+    String marca; */
 
-    public vaso(int capacidad, String material) {
-        this.ml_capacidad = capacidad;
-        this.material = material;
-        this.ml_liquido = 0;
-    }
-
-    public vaso() {
-        this(20, "cristal");
-    }
-
-    public int getContenido() {
+    
+    /**  els vasos siguin de 8 ml de plastic
+     * Fer getters
+     * Fer un metode per rellenar amb els ml que li vingui d'entrada
+     * Verter de 2 en 2 ml
+     * santHilari()*/
+    public int getMlLiquido(){
         return ml_liquido;
     }
 
-    public void llenar(int cantidad) {
+    public int getCapacidad(){
+        return ml_capacidad;
+    }
 
-        if (ml_liquido + cantidad > ml_capacidad) {
-            ml_liquido = ml_capacidad;
-        } else {
-            ml_liquido += cantidad;
-        }
+    public String getMaterial(){
+        return material;
     }
 }
+
+
