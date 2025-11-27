809927c9b6592eb833e182c99aa74b549aa332b9
RaulCasass <149099769+RaulCasass@users.noreply.github.com>
2025-11-28 00:54:44 +0100

    20251128

---
+++ 20251128/RaulCasas/Persona.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author rasu2
+ */
+public class Persona {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+
+    public String getDni() {
+        return dni;
+    }
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean esMayorEdad() {
+        return edad >= 18;
+    }
+
+    public void imprime() {
+        System.out.println(nombre + " " + apellidos +  " con DNI " + dni + " tiene " + edad + " años.");
+        
+         if (esMayorEdad()) {
+            System.out.println("Es mayor de edad ");
+        } else {
+            System.out.println("No es mayor de edad ");
+        }
+    }
+}
