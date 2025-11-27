faa7944b0f1c10c34ba22362f14a71b189ec98d5
maquintanarp-tech <maquintanarp@gmail.com>
2025-11-27 21:21:45 +0100

    Add files via upload

---
+++ 20251128/MarceloQuintana/Persona.java
@@ -0,0 +1,51 @@
+package Objeto;
+
+public class Persona {
+
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public String getDni() {
+        return dni;
+    }
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean esMayorEdad() {
+        return edad >= 18;
+    }
+
+    public void imprime() {
+        System.out.println(nombre + " " + apellidos +  " con DNI " + dni + " tiene " + edad + " años.");
+        
+         if (esMayorEdad()) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("No es mayor de edad");
+        }
+    }
+}
