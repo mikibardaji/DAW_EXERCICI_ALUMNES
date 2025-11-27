4e9f1711c008d7cbe289dd1eadcf9d9bded19605
alexvazoliv-afk <alexvaz.oliv@gmail.com>
2025-11-27 22:40:32 +0100

    20251128

---
+++ 20251128/Persona.java
@@ -0,0 +1,36 @@
+package Objetos;
+
+public class Persona {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Persona(String dni, String nombre, String apellidos, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.dni = "Sin DNI";
+    }
+
+    public String getDni() { return dni; }
+    public String getNombre() { return nombre; }
+    public String getApellidos() { return apellidos; }
+    public int getEdad() { return edad; }
+
+    public void setDni(String dni) { this.dni = dni; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+    public void setEdad(int edad) { this.edad = edad; }
+
+    public boolean esMayorDeEdad() {
+        return edad >= 18;
+    }
+}
