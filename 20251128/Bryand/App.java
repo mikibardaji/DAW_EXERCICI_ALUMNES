76811f7bb18b0e9e36751865e7b9e2ce38337432
bryand <48119387+bryanddavid@users.noreply.github.com>
2025-11-28 10:34:05 +0100

    20251128

--- 20251128/Bryand/App.java
+++ 20251128/Bryand/App.java
@@ -6,32 +6,30 @@ public class App {
       Persona pers1 = new Persona();
         Persona pers2 = new Persona();  
 
-        System.out.println("DNI de primera persona");
-        pers1.setDni(sc.nextLine());
-        System.out.println("Nombre de primera persona");
+                System.out.println("DNI de la primera persona");
+                pers1.setDni(sc.nextLine());
+                System.out.println("Nombre de la primera persona");
         pers1.setNombre(sc.nextLine());
-        System.out.println("Apellido de primera persona");
+                System.out.println("Apellido de la primera persona");
         pers1.setApellidos(sc.nextLine());
-        System.out.println("Edad priemra persona");
+                    System.out.println("Edad de la primera persona");
         pers1.setEdad(sc.nextInt());
-       
-       
-
-
-        System.out.println("DNI de segunda persona");
-        pers2.setDni(sc.nextLine());
-        System.out.println("Nombre de segunda persona");
-        pers2.setNombre(sc.nextLine());
-        System.out.println("Apellido de segunda persona");
+        
+            System.out.println("DNI de la segunda persona");
+                pers2.setDni(sc.nextLine());
+            System.out.println("Nombre de la segunda persona");
+                        pers2.setNombre(sc.nextLine());
+                System.out.println("Apellido de la segunda persona");
         pers2.setApellidos(sc.nextLine());
-        System.out.println("Edad segunda persona");
+                System.out.println("Edad de la segunda persona");
         pers2.setEdad(sc.nextInt());
-
+        
        
         pers1.imprime();
         pers2.imprime();
 
     }
+
 
 
 
