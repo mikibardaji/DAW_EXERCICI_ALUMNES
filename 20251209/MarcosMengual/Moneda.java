d2ad80c21c85267854ce17db483c1fc6569cc9a1
marcosmengual8 <marcosmengual8@gmail.com>
2025-12-09 00:50:46 +0100

    20251209

--- 20251209/MarcosMengual/Moneda.java
+++ 20251209/MarcosMengual/Moneda.java
@@ -1,5 +1,23 @@
 package Joc;
 
-public class Moneda {
+import java.util.Random;
 
+
+public class Moneda {
+    private int valor;
+
+    public Moneda() {
+        Random random = new Random();
+        this.valor = random.nextInt(101);
+    }
+
+    public boolean hasPowerUp() {
+        long mls = System.currentTimeMillis();
+        return mls % 3 == 0;
+    }
+
+    public int getValor() {
+        return valor;
+    }
 }
+
