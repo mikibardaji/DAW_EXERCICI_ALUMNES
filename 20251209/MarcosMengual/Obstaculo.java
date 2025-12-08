6e1f782faa499de6926da64f7b9fe419a81998b9
marcosmengual8 <marcosmengual8@gmail.com>
2025-12-09 00:49:56 +0100

    20251209

--- 20251209/MarcosMengual/Obstaculo.java
+++ 20251209/MarcosMengual/Obstaculo.java
@@ -1,5 +1,56 @@
 package Joc;
 
-public class Obstaculo {
+import java.util.Random;
 
+public class Obstaculo {
+    
+    public static final int MIN_BARRERA = 1;
+    public static final int MAX_BARRERA = 5;
+
+    public static final int MIN_TREN = 5;
+    public static final int MAX_TREN = 10;
+    
+   
+    private String tipus;
+    private int dificultat;
+
+    public String getTipus() {
+        return tipus;
+    }
+
+    public int getDificultat() {
+        return dificultat;
+    }
+    
+    
+        public Obstaculo(String tipus, int dificultat) {
+        this.tipus = tipus;
+        this.dificultat = dificultat;
+    }
+    
+     public Obstaculo() {
+        Random rand = new Random();
+
+      
+        if (rand.nextBoolean()) {
+            tipus = "barrera";
+            dificultat = rand.nextInt(MAX_BARRERA - MIN_BARRERA + 1) + MIN_BARRERA;
+        } else {
+            tipus = "tren";
+            dificultat = rand.nextInt(MAX_TREN - MIN_TREN + 1) + MIN_TREN;
+        }
+               
+    }
+     
+      public void interactuar(Jugador jugador) {
+        if (jugador == null) return;
+
+        jugador.chocar(this);
+    }
+     
+         public static Obstaculo obstacleMaxim() {
+     
+        return new Obstaculo("tren", MAX_TREN);
+    }    
 }
+
