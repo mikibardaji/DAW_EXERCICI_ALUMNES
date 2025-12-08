62d930f629cfa044c057282e740fb73bd2da0804
44danif <danielf.m3007@gmail.com>
2025-12-08 21:42:40 +0100

    Add files via upload

---
+++ 20251209/FiguereoDaniel/Obstacle.java
@@ -0,0 +1,64 @@
+package Joc;
+import java.util.Random;
+
+public class Obstacle {
+    
+    public static final int MIN_BARRERA = 1;
+    public static final int MAX_BARRERA = 5;
+
+    public static final int MIN_TREN = 5;
+    public static final int MAX_TREN = 10;
+    
+    
+    
+    private String tipus;
+    private int dificultat;
+
+    public String getTipus() {
+        return tipus;
+    }
+
+    public int getDificultat() {
+        return dificultat;
+    }
+    
+    
+        public Obstacle(String tipus, int dificultat) {
+        this.tipus = tipus;
+        this.dificultat = dificultat;
+    }
+    
+     public Obstacle() {
+        Random rand = new Random();
+
+      
+        if (rand.nextBoolean()) {
+            tipus = "barrera";
+            dificultat = rand.nextInt(MAX_BARRERA - MIN_BARRERA + 1) + MIN_BARRERA;
+        } else {
+            tipus = "tren";
+            dificultat = rand.nextInt(MAX_TREN - MIN_TREN + 1) + MIN_TREN;
+        }
+        
+        
+        
+        
+        
+    }
+     
+      public void interactuar(Jugador jugador) {
+        if (jugador == null) return;
+
+        jugador.xocar(this);
+    }
+     
+         public static Obstacle obstacleMaxim() {
+     
+        return new Obstacle("tren", MAX_TREN);
+    }
+     
+     
+     
+     
+    
+}
