7b36638d2565cb4492c673844da2e2a20b4ab18e
Anass3731 <aoussar371@gmail.com>
2025-12-14 20:37:38 +0100

    20121216

---
+++ 20121216/Anass.B/masajista.java
@@ -0,0 +1,19 @@
+public class masajista {
+private int id_massajista;
+private String nombre;
+private int edad;
+private String titulacion;
+
+public void Viajar(){
+
+};
+
+public void Concentrarse(){
+
+};
+
+public void dar_masaje(){
+
+};
+
+}
