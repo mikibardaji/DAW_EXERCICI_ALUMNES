db5472f2035ce40d89669ea7e5bd78a034769f65
Mohamed1875575 <44614604+Mohamed1875575@users.noreply.github.com>
2025-12-16 00:19:00 +0100

    Create futbolista.java

---
+++ 20121216/Rifi/futbolista.java
@@ -0,0 +1,23 @@
+public class futbolista {
+private int id_futbolista;
+private String nombre;
+private int edad;
+private int goles;
+
+public void Viajar(){
+
+};
+
+public void Concentrarse(){
+
+};
+
+public void Jugarpartido(){
+
+};
+
+public void entrenar(){
+
+};
+
+}
