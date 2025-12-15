b5f5e943cb1ee7cba9e97730a7cfac6d51c92d49
maquintanarp-tech <maquintanarp@gmail.com>
2025-12-15 18:38:01 +0100

    20121216

---
+++ 20121216/MarceloQuintana/Presidente.java
@@ -0,0 +1,28 @@
+package Objetos;
+
+public class Presidente {
+    private int id;
+    private String nombre;
+    private int edad;
+    private int anys_cargo;
+
+        private void Viajar(){
+        System.out.println("Viajar");
+}
+         private void Concentrarse(){
+        System.out.println("Concentrarse");
+}
+
+
+        private void Ir_palco_partido(){
+        System.out.println("Ir al palco");
+}
+
+         private void tratar_contrato(){
+         System.out.println("Tratar el contrato");
+
+
+}
+}
+
+
