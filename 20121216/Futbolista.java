b011aa9923f1b328031fc6a33d8f31e8f4a8705f
alexvazoliv-afk <alexvaz.oliv@gmail.com>
2025-12-15 22:38:10 +0100

    Update Futbolista.java

--- 20121216/Futbolista.java
+++ 20121216/Futbolista.java
@@ -1,5 +1,39 @@
+
 package Objetos;
 
 public class Futbolista {
+
+    private int id;
+    private String nombre;
+    private int edad;
+    private int goles;
+
+    private void Viajar(){
+
+System.out.println("viajar");
+
+    }
+
+
+    private void Concentarse(){
+
+        System.out.println("Concentrarse");
+    }
+
+
+
+    private void Jugarpartido(){
+
+System.out.println("JugarPartido");
+
+
+    }
+
+
+    private void entrenar(){
+
+        System.out.println("Entrenar");
+    }
+
 
 }
