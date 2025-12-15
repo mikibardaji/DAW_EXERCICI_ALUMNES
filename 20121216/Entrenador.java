55b00eac23e67ac78b51553742651794c276f665
alexvazoliv-afk <alexvaz.oliv@gmail.com>
2025-12-15 22:37:32 +0100

    Add files via upload

---
+++ 20121216/Entrenador.java
@@ -0,0 +1,35 @@
+package Objetos;
+
+public class Entrenador {
+
+
+
+    private int id ;
+    private String nombre;
+    private int edad;
+    private boolean tiene_titulo;
+
+
+    private void Viajar(){
+
+        System.out.println("Viajar");
+    }
+
+     private void Concentrarse(){
+
+        System.out.println("Concentrarse");
+    }
+    private void dirigirpartido(){
+
+        System.out.println("Dirigir partido");
+
+    }
+
+    private void entrenar(){
+
+        System.out.println("Entrenar");
+
+    }
+
+
+}
