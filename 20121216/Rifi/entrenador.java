1edb7cee0ff71a6b9b5f1c6c0b1a104a00d34595
Mohamed1875575 <44614604+Mohamed1875575@users.noreply.github.com>
2025-12-16 00:18:15 +0100

    Create entrenador.java

---
+++ 20121216/Rifi/entrenador.java
@@ -0,0 +1,23 @@
+public class entrenador {
+private int id_entrenador;
+private String nombre;
+private int edad;
+private boolean tiene_titulo;
+
+public void Viajar(){
+
+};
+
+public void Concentrarse(){
+
+};
+
+public void dirigirpartido(){
+
+};
+
+public void entrenar(){
+
+};
+
+}
