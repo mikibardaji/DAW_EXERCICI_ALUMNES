43ad4112cde7b033af3fbe4cabb77f363381b371
RaulCasass <149099769+RaulCasass@users.noreply.github.com>
2025-12-15 21:21:28 +0100

    20121216

---
+++ 20121216/Entrenador.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author rasu2
+ */
+public class Entrenador {
+     private int id ;
+    private String nombre;
+    private int edad;
+    private boolean tiene_titulo;
+
+
+    private void Viajar(){
+
+        System.out.println("Viajar");
+    }
+
+     private void Concentrarse(){
+
+        System.out.println("Concentrarse");
+    }
+    private void dirigirpartido(){
+
+        System.out.println("Dirigir partido");
+
+    }
+    
+    private void entrenar(){
+
+        System.out.println("Entrenar");
+
+    }
+}
