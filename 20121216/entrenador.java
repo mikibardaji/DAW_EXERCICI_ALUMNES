3b4e66040fd890e87bdcb3ec722dacc005156ac0
mgundin7 <166124365+mgundin7@users.noreply.github.com>
2025-12-15 23:34:06 +0100

    Add files via upload

---
+++ 20121216/entrenador.java
@@ -0,0 +1,25 @@
+package Objetos;
+
+public class entrenador {
+    private int id;
+    private String nombre;
+    private int edad;
+    private boolean tiene_titulo;
+
+    private void Viajar() {
+        System.out.println("Viajar");
+    }
+
+    private void Concentrarse() {
+        System.out.println("Concentrarse");
+    }
+
+    private void dirigirpartido() {
+        System.out.println("Dirigir partido");
+    }
+
+    private void entrenar() {
+        System.out.println("Entrenar");
+
+    }
+}
---
+++ 20121216/presidente.java
@@ -0,0 +1,25 @@
+package Objetos;
+
+public class presidente {
+    private int id;
+    private String nombre;
+    private int edad;
+    private int anys_cargo;
+
+    private void Viajar() {
+        System.out.println("Viajar");
+    }
+
+    private void Concentrarse() {
+        System.out.println("Concentrarse");
+    }
+
+    private void Ir_palco_partido() {
+        System.out.println("Ir al palco");
+    }
+
+    private void tratar_contrato() {
+        System.out.println("Tratar el contrato");
+
+    }
+}
