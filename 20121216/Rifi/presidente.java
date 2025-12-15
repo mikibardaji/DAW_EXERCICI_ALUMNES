b3c3eed15de0325c19dd7432c32bb108a2f82ee9
Mohamed1875575 <44614604+Mohamed1875575@users.noreply.github.com>
2025-12-16 00:20:10 +0100

    Create presidente.java

---
+++ 20121216/Rifi/presidente.java
@@ -0,0 +1,23 @@
+public class presidente {
+private int id_presidente;
+private String nombre;
+private int edad;
+private int años_cargo;
+
+
+public void Viajar(){
+
+};
+
+public void Concentrarse(){
+
+};
+
+public void Ir_palco_partido(){
+
+};
+
+public void tratar_contrato(String Equipo_Fubtol){
+
+};
+}
