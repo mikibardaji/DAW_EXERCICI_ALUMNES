29abdc8f30896d76383389e2348f6f813292527a
marcosmengual8 <marcosmengual8@gmail.com>
2025-12-16 00:24:38 +0100

    20121216

---
+++ 20121216/MarcosMengual/futbolista.java
@@ -0,0 +1,24 @@
+package Objetos;
+
+public class futbolista {
+    private int id;
+    private String nombre;
+    private int edad;
+    private int goles;
+
+    private void Viajar(){
+    System.out.println("viajar");
+
+}
+    private void Concentarse(){
+    System.out.println("Concentrarse");
+    
+}
+    private void Jugarpartido(){
+    System.out.println("JugarPartido");
+
+}
+    private void entrenar(){
+    System.out.println("Entrenar"); 
+}
+}
