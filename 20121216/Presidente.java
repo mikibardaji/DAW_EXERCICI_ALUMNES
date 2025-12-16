e8b618aa369cf5e359b67e62000c6b5992b4601d
Goyo <163832659+Goyoisinter@users.noreply.github.com>
2025-12-16 04:24:48 +0100

    Add files via upload

---
+++ 20121216/Presidente.java
@@ -0,0 +1,24 @@
+package objetos;
+
+public class Presidente {
+    private int id;
+    private int nombre;
+    private int edad;
+    private int años_cargo;
+
+    private void Viajar() {
+        System.out.println("Viajar");
+    }
+
+    private void Concentrarse() {
+        System.out.println("Concentrarse");
+    }
+
+    private void irPalcoPartido() {
+        System.out.println("irPalcoPartido");
+    }
+
+    private void tratarContrato() {
+        System.out.println("tratarContrato");
+    }
+}
