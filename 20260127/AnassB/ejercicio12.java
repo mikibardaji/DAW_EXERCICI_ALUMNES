5a3bad8d237ba39835a27aefde5a969a2dcc218d
Anass3731 <aoussar371@gmail.com>
2026-01-27 11:40:21 +0100

    Create ejercicio12.java

---
+++ 20260127/AnassB/ejercicio12.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ejercicio12 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        int numerosEnteros [];
+        numerosEnteros = new int[10];
+
+        
+        String opcion;
+        
+            
+        do{
+        System.out.println("Que opcion quieres hacer?");
+        System.out.println("A->  Mostrar valores");
+        System.out.println("B-> Introducir valor");
+        System.out.println("C-> Salir ");
+        opcion = sc.nextLine();
+
+        if (opcion.equalsIgnoreCase("a")) {
+             mostrarNumeros(numerosEnteros);
+        } else if (opcion.equalsIgnoreCase("b")) {
+            cambiarValor(numerosEnteros);
+        } 
+        }while (!opcion.equalsIgnoreCase("c"));
+    }
+    
+    private static void mostrarNumeros(int [] numerosEnteros){
+        for (int i = 0; i < numerosEnteros.length; i++){
+            System.out.print(numerosEnteros[i] + "-");
+         }
+         System.out.println();
+    }
+
+     private static void cambiarValor (int [] numerosEnteros){
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Que posicion de valor quieres cambiar?");
+        int posicionCambio = sc.nextInt();
+
+        System.out.println("Que valor le quieres cambiar a la posicion?");
+        int valorCambio = sc.nextInt();
+
+        numerosEnteros[posicionCambio-1] = valorCambio;     
+        }
+
+}
