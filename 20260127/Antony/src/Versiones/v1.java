9cff9de7a0b72a902a9df4dd42f1023847df3317
Marck Montaño <marck.dplan@gmail.com>
2026-01-27 11:32:42 +0100

    Add files via upload

---
+++ 20260127/Antony/src/Versiones/v1.java
@@ -0,0 +1,33 @@
+package Versiones;
+
+import java.util.Scanner;
+
+public class v1 {
+/*V1.0 .- Crea un programa que cree un array de 10 enteros y luego muestre el siguiente menú con distintas opciones: a. Mostrar valores. b. Introducir valor. c. Salir. 
+La opción ‘a’ mostrará todos los valores por pantalla. La opción ‘b’ pedirá un valor V y una posición P, luego escribirá V en la posición P del array. 
+El menú se repetirá indefinidamente hasta que el usuario elija la opción ‘c’ que terminará el programa.
+
+V1.1 .- Cámbialo para que la posición que te ponga el usuario sea la posición natural, por ejemplo, si te escribe la posición 3, quiere decir que lo quiere poner en la tercera posición del array (en este caso índice = 2). 
+Siempre es una posición menos. Valida que si te ponen más posiciones que capacidad tiene el array, siga preguntando hasta que la posición sea válida (o sea, no supere la length).
+
+V1.2 .- Cuando pongas la posición, mira antes si esta posición no estaba informada (no está informada es que la posición está a 0). 
+Si esa posición está informada, pregunte: ¿Quieres machacar el resultado anterior (s/n), o si queréis (SI/NO), o si queréis (Sí-1, No-0)? La que sepáis preguntar, no me importar */
+
+private static int enteros()
+{
+    Scanner sc = new Scanner(System.in);
+    boolean a=0;
+    int b,c;
+    System.out.println("A , B , C");
+    String opciones = sc.next();
+do
+if (a) {
+    
+}
+
+}
+
+
+
+
+}
