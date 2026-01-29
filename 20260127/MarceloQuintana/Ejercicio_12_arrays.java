149155a56b77a197d5dbc4a65752845f4d3c822b
maquintanarp-tech <maquintanarp@gmail.com>
2026-01-29 09:44:52 +0100

    20260127

--- 20260127/MarceloQuintana/Ejercicio_12_arrays.java
+++ 20260127/MarceloQuintana/Ejercicio_12_arrays.java
@@ -1,68 +1,54 @@
+
+Marcelo Quintana <maquintanarp@gmail.com>
+mar, 27 ene, 11:29 (hace 2 días)
+para mí
+
 import java.util.Scanner;
 
 public class Ejercicio_12_arrays {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+          Scanner sc = new Scanner(System.in);
+
         int[] array = new int[10];
         char opcion;
 
-        do {
-            mostrarMenu();
-            opcion = leerOpcion(sc);
+            do {
+            System.out.println("--- MENÚ ---");
+            System.out.println("a. Mostrar valores");
+            System.out.println("b. Introducir valor");
+            System.out.println("c. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = sc.next().charAt(0);
+        
+          switch (opcion) {
+                     case 'a':
+                    System.out.println("Valores del array:");
+                    for (int i = 0; i < array.length; i++) {
+                        System.out.println("Posición " + i + ": " + array[i]);
+                    }
+                    break;
+                   case 'b':
+                    System.out.print("Introduce un valor: ");
+                    int v = sc.nextInt();
+                    System.out.print("Introduce una posición : ");
+                    int p = sc.nextInt();
+                    
+                      if (p >= 0 && p < array.length) {
+                        array[p] = v;
+                        System.out.println("Valor guardado correctamente.");
+                    } else {
+                        System.out.println("Posición incorrecta.");
+                    }
+                    break;
 
-            switch (opcion) {
-                case 'a':
-                    mostrarValores(array);
-                    break;
-                case 'b':
-                    introducirValor(array, sc);
-                    break;
                 case 'c':
                     System.out.println("Saliendo...");
                     break;
+
                 default:
                     System.out.println("Opción no válida.");
+                    break;
             }
         } while (opcion != 'c');
-
-
-    }
-
- 
-    public static void mostrarMenu() {
-        System.out.println("--- MENÚ ---");
-        System.out.println("a. Mostrar valores");
-        System.out.println("b. Introducir valor");
-        System.out.println("c. Salir");
-        System.out.print("Elige una opción: ");
-    }
-
-    
-    public static char leerOpcion(Scanner sc) {
-        return sc.next().charAt(0);
-    }
-
-
-    public static void mostrarValores(int[] array) {
-        System.out.println("Valores del array:");
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Posición " + i + ": " + array[i]);
-        }
-    }
-
-    public static void introducirValor(int[] array, Scanner sc) {
-        System.out.print("Introduce un valor: ");
-        int valor = sc.nextInt();
-
-        System.out.print("Introduce una posición: ");
-        int posicion = sc.nextInt();
-
-        if (posicion >= 0 && posicion < array.length) {
-            array[posicion] = valor;
-            System.out.println("Valor guardado correctamente.");
-        } else {
-            System.out.println("Posición incorrecta.");
-        }
-    }
+    }      
 }
