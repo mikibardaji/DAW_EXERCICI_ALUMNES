a0ddb835a0248d2276089e260d33f8a0b1b7dce0
bryand <48119387+bryanddavid@users.noreply.github.com>
2026-01-30 09:17:51 +0100

    20260127

---
+++ 20260127/Bryand/PerCasa.java
@@ -0,0 +1,23 @@
+InicializarJuego()
+Prepara les caixes i els premis.
+
+ElegirCaja()
+El jugador escull la seva caixa.
+
+AbrirCaja()
+Obre una caixa i mostra el premi.
+
+MostrarPremiosRestantes()
+Mostra els premis que queden.
+
+LlamadaBanca()
+Simula la trucada de la banca.
+
+CalcularOferta()
+Calcula l’oferta de la banca.
+
+AceptarORechazar()
+El jugador accepta o rebutja l’oferta.
+
+FinalJuego()
+Mostra el resultat final.
