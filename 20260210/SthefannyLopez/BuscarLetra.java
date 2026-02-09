2f7ccc2fd0e05739afe449f25dad7af2b30b8c10
sthefannylopez <srlp2804@gmail.com>
2026-02-09 23:15:48 +0100

    Update BuscarLetra.java

--- 20260210/SthefannyLopez/BuscarLetra.java
+++ 20260210/SthefannyLopez/BuscarLetra.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class BuscarLetra {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escribe una frase: ");
+        String frase = sc.nextLine();
+
+        System.out.print("Dime una letra: ");
+        char letra = sc.next().charAt(0);
+
+        boolean hay = false;
+
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+
+            if (Character.toLowerCase(c) == Character.toLowerCase(letra)) {
+                System.out.println("La letra aparece en la posición:" + i);
+                hay = true;
+            }
+        }
+
+        if (!hay) System.out.println("No aparece la letra '" + letra + "' en la frase.");
+
+        sc.close();
+    }
+}
