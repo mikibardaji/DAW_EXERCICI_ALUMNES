7024a06551ed4ccea47ce70cece16223a7b97c4d
mgundin7 <166124365+mgundin7@users.noreply.github.com>
2026-02-13 10:43:25 +0100

    Add files via upload

---
+++ 20260213/Gato.java
@@ -0,0 +1,38 @@
+package Objetos;
+
+public class Gato {
+
+    private String nombre;
+    private int edad;
+
+    public Gato(int edad, String nombre) throws Exception {
+        setEdad(edad);
+        setNombre(nombre);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) throws Exception {
+        if (nombre == null || nombre.length() < 3) {
+            throw new Exception("El nombre del gato debe tener al menos 3 caracteres");
+        }
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) throws Exception {
+        if (edad < 0) {
+            throw new Exception("La edad del gato no puede ser negativa");
+        }
+        this.edad = edad;
+    }
+
+    public String imprimir() {
+        return "Nombre: " + nombre + " Edad: " + edad;
+    }
+}
