6e26b5fe250796869650bb39a8726f7606245f37
Jeremy610x <94995811+Jeremy610x@users.noreply.github.com>
2026-03-05 19:52:52 +0100

    20260303

---
+++ 20260303/JeremyPanchana/Modelo/InvalidGeneroException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class InvalidGeneroException extends Exception{
+        public InvalidGeneroException() {
+        super("Genero incorrecto, lo paso a terrro ");
+    }
+}
