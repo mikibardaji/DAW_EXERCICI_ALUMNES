0398b90a2d421d7913adabc4d716158168c7e0f3
maquintanarp-tech <maquintanarp@gmail.com>
2026-03-09 09:33:02 +0100

    20260303

---
+++ 20260303/MarceloQuintana/Pelicula.java
@@ -0,0 +1,81 @@
+package Modelo;
+
+public class Pelicula {
+     private int codiPel;
+    private String Titol;
+    private String genere;
+    private boolean segundaParte;
+
+    public Pelicula(int codiPel, String Titol, String genere, boolean segundaParte) {
+        this.codiPel = codiPel;
+        this.Titol = Titol;
+        this.genere = genere;
+        this.segundaParte = segundaParte;
+    }
+
+    public int getCodiPel() {
+        return codiPel;
+    }
+
+    public void setCodiPel(int codiPel) {
+        this.codiPel = codiPel;
+    }
+
+    public String getTitol() {
+        return Titol;
+    }
+
+    public void setTitol(String Titol) {
+        this.Titol = Titol;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) throws InvalidGeneroException {
+        //genere = genere.toUpperCase();
+        if ("WESTERN".equalsIgnoreCase(genere) 
+                || "CIENCIA FICCIO".equalsIgnoreCase(genere)
+                || "TERROR".equalsIgnoreCase(genere) )
+        {
+            this.genere = genere;
+        }
+        else
+        {
+            throw new InvalidGeneroException();
+            
+        }
+    }
+
+    public boolean getSegundaParte() {
+        return segundaParte;
+    }
+
+    public void setSegundaParte(boolean segundaParte) {
+        this.segundaParte = segundaParte;
+    }
+
+    @Override
+    public String toString() {
+        return  Titol + "  genere " + genere + ", segundaParte=" + segundaParte;
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Pelicula)) {
+            return false;
+        }
+        final Pelicula other = (Pelicula) obj;
+        return this.codiPel == other.codiPel;
+    }
+    
+}
---
+++ 20260303/MarceloQuintana/PeliculaDAO.java
@@ -0,0 +1,133 @@
+package Modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeliculaDAO {
+       List<Pelicula> cataleg;
+
+    public PeliculaDAO() {
+        this.cataleg = new ArrayList<>();
+        this.afegirPelisInicials();
+    }
+
+    private void afegirPelisInicials() {
+        cataleg.add(new Pelicula(1, "Alien", "TERROR", true));
+        cataleg.add(new Pelicula(2, "Matrix", "CIENCIA FICCIO", true));
+        cataleg.add(new Pelicula(3, "Salvar al Soldado Ryan", "WESTERN", false));
+        cataleg.add(new Pelicula(4, "Top Secret", "Comica", false));
+    }
+
+    public boolean addPeli(Pelicula anyadir) {
+        /*validaciones
+        por ejemplo no queremos pelis de 18 años
+        lanzaria excepcion (falta atributo edad en pelis)*/
+        return cataleg.add(anyadir);
+        
+    }
+
+    public List<Pelicula> allPelis() {
+        return cataleg;
+    }
+
+    public List<Pelicula> AllPelisSecondPart(boolean segunda) {
+        List<Pelicula> seleccionadas = new ArrayList<>();
+        
+        for (int i = 0; i < cataleg.size(); i++) {
+            if (cataleg.get(i).getSegundaParte()==segunda)
+            { //coincide lo añado
+                seleccionadas.add(cataleg.get(i));
+            }
+        }
+        return seleccionadas;
+    }
+    
+    
+    public List<Pelicula > filtrarGenere(String genere ){
+    
+    List<Pelicula > seleccionadas = new ArrayList<>();
+    
+    
+        for (Pelicula peli : cataleg) {
+            
+            if (peli.getGenere().equalsIgnoreCase(genere));
+            
+            
+            seleccionadas.add(peli);
+            
+            
+        }
+        
+        
+        return seleccionadas;
+    
+    }
+    
+    
+    
+    
+    public Pelicula buscarporId(int id){
+    
+    
+        for (Pelicula peli : cataleg) {
+            if (peli.getCodiPel() == id){
+            return peli;
+            }
+        }
+    return null;
+    }
+
+    
+    
+    
+    public List <Pelicula > buscarporNombre(String nombre ){
+    
+    List <Pelicula > encontradas = new ArrayList<>();
+    
+    for(Pelicula peli : cataleg){
+    
+    if (peli.getTitol().equalsIgnoreCase(nombre)){
+    
+    encontradas.add(peli);
+    }
+    }
+    
+    return encontradas;
+    }
+  
+    
+    
+    
+    
+    public boolean borrarPerId(int id){
+        Pelicula peli = buscarporId(id);
+        
+        
+        if (peli != null){
+    
+    
+        cataleg.remove(peli);
+        }
+        return false;
+    
+    
+    }
+    
+    public int borrarPorNombre (String nombre ){
+    
+    int contador = 0;
+    
+        for (int i = 0; i < cataleg.size(); i++) {
+            if (cataleg.get(i).getTitol().equalsIgnoreCase(nombre)){
+            
+            cataleg.remove(i);
+            contador++;
+            i--;
+            }
+        }
+    
+    return  contador ;
+    }
+    
+    
+}
