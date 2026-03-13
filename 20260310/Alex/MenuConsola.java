5e207451f5a71e130051350d372179e9376c698b
alexvazoliv-afk <alexvaz.oliv@gmail.com>
2026-03-13 10:27:53 +0100

    20260310

--- 20260310/Alex/MenuConsola.java
+++ 20260310/Alex/MenuConsola.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Modelo;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- *
- * @author Jeremy
- */
+
 public class MenuConsola {
 
   private String titol ;
@@ -72,3 +66,4 @@ public class MenuConsola {
         return opcio;
     }
 }
+
