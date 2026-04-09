f621ddb52f1c58cd6514528b1c25de3976f48e78
marcosmengual8 <marcosmengual8@gmail.com>
2026-04-10 01:22:21 +0200

    Add files via upload

---
+++ 20260407/MarcosMengual/DbConnect.java
@@ -0,0 +1,36 @@
+package persistenciamodelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author mabardaji
+ */
+public class DbConnect {
+    
+    static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String PROTOCOL = "jdbc:mysql:";
+    static final String HOST = "127.0.0.1"; //localhost
+    static final String BD_NAME = "dbmovies";
+    static final String USER = "usrmovies";
+    static final String PASSWORD = "pswmovies";
+    static final String PARAMS = "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    
+    
+    public static void loadDriver() throws ClassNotFoundException {
+        Class.forName(DRIVER);
+    }  
+
+        /**
+        * gets and returns a connection to database
+        * @return connection
+        * @throws java.sql.SQLException
+        */
+    public static Connection getConnection() throws SQLException {
+        final String BD_URL = String.format("%s//%s/%s?%s", PROTOCOL, HOST, BD_NAME, PARAMS);
+        Connection conn = null;
+        conn = DriverManager.getConnection(BD_URL, USER, PASSWORD);
+        return conn;
+    }
+}
