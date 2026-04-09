6b75c1952d9ac90f477ccc7c3c8f267d59e8d022
marcosmengual8 <marcosmengual8@gmail.com>
2026-04-10 01:24:38 +0200

    20260407

---
+++ 20260407/MarcosMengual/Modelo/InterfaceDAO.java
@@ -0,0 +1,20 @@
+package Modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author mabardaji
+ */
+public interface InterfaceDAO {
+    
+    public boolean addPeli(Pelicula anyadir);
+    public List<Pelicula> allPelis();
+    public List<Pelicula> AllPelisSecondPart(boolean segunda);
+    public boolean borrarPelicula(Pelicula fakeDelete) ;
+    public Pelicula findPeli(Pelicula fakeSearch) throws InvalidIdPeliculaException;
+    public List<Pelicula> findPelisGenero(String generoSearch) ;
+    public List<Pelicula> findPelisGenero2(Pelicula generoSearch);
+    public int borrarPeliculasNombre(String tituloDelete);
+    public boolean CambiarNombrePelicula(Pelicula searchPelicula);
+}
