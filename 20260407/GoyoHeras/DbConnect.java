8b33daeab77b864e955c2daf54698892529f74d9
Goyo <163832659+Goyoisinter@users.noreply.github.com>
2026-04-10 03:10:28 +0200

    Add files via upload

---
+++ 20260407/GoyoHeras/DbConnect.java
@@ -0,0 +1,26 @@
+package PersistenciaModelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnect {
+    static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String PROTOCOL = "jdbc:mysql:";
+    static final String HOST = "127.0.0.1";
+    static final String BD_NAME = "dbpeliculas";
+    static final String USER = "usrcountry";
+    static final String PASSWORD = "pswcountry";
+    static final String PARAMS = "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public static void loadDriver() throws ClassNotFoundException {
+        Class.forName(DRIVER);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        final String BD_URL = String.format("%s//%s/%s?%s", PROTOCOL, HOST, BD_NAME, PARAMS);
+        Connection conn = null;
+        conn = DriverManager.getConnection(BD_URL, USER, PASSWORD);
+        return conn;
+    }
+}
