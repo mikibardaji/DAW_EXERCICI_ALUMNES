eb55e38335f75cac0446b28ff4c97638fd7d84fc
marcosmengual8 <marcosmengual8@gmail.com>
2026-05-03 19:15:29 +0200

    20260428

---
+++ 20260428/VentanaPrincipal.java
@@ -0,0 +1,50 @@
+package introduccionDatosLuchador;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame {
+
+    public void FinestraPrincipal() throws HeadlessException{
+    
+        initComponentsTitle();
+    
+    initContainers();
+    
+    }
+
+    private void initComponentsTitle() {
+
+        this.setTitle("Introduccio Dades Luchador ");
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setSize(1200, 500);
+        this.setLocationRelativeTo(null);
+          
+
+    }
+
+    private void initContainers() {
+
+        
+        Container principal = this.getContentPane();
+        
+        principal.setLayout(new GridLayout(1,2));
+        
+        PanelDatosEntrada datos = new PanelDatosEntrada();
+
+        
+        
+        principal.add(datos,BorderLayout.CENTER);
+        
+        PanelBotonEntrada botones = new PanelBotonEntrada();
+        
+        botones.setPreferredSize(new Dimension(0,150));
+        principal.add(botones);
+        
+        
+     }
+}
