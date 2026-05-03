b9b4cf04d3b0342b553e03435898ca8f0d4bfd99
alexvazoliv-afk <alexvaz.oliv@gmail.com>
2026-05-03 21:40:47 +0200

    20260428
    20260428

---
+++ 20260428/AlexVazquez/PanelBotonesEntrada.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package introducciodadeslluitador;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class PanelBotonesEntrada extends JPanel{
+    JButton boton1 ;
+    JButton boton2 ;
+    JButton boton3 ;
+    JButton boton4;
+    JButton boton5;
+       public PanelBotonesEntrada() {
+        initComponents();
+        inicializarCampos();
+        
+    }
+
+    private void initComponents() {
+         this.setLayout(new GridLayout(5,1));
+         boton1 = new JButton();
+        this.add(boton1); 
+        
+        
+         boton2 = new JButton();
+        this.add(boton2);
+           boton3 = new JButton();
+        this.add(boton3);
+           boton4 = new JButton();
+        this.add(boton4);
+           boton5 = new JButton();
+        this.add(boton5);
+        
+        
+
+    }
+
+    private void inicializarCampos() {
+
+        boton1.setText(" Alta Luchador ");
+         boton2.setText(" Luchador eficiencia  ");
+           boton3.setText(" Combate Luchador ");
+            boton4.setText(" Buscar  Luchador ");
+             boton5.setText(" Otra opcion ");
+
+    }
+}
