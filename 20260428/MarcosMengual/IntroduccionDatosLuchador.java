c0f9f7af34394a9374f14123b9ee2d646ca7edbe
marcosmengual8 <marcosmengual8@gmail.com>
2026-05-03 19:16:27 +0200

    20260428

---
+++ 20260428/MarcosMengual/IntroduccionDatosLuchador.java
@@ -0,0 +1,12 @@
+package introduccionDatosLuchador;
+
+public class IntroduccionDatosLuchador {
+
+     public static void main(String[] args) {
+        
+        
+        VentanaPrincipal app = new  VentanaPrincipal();
+        
+        app.setVisible(true);
+    }
+}
