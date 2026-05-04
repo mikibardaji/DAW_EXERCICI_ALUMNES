6a984eb75b3ceaa0810a3696bfbccb48595e16cf
Marck Montaño <marck.dplan@gmail.com>
2026-05-04 23:40:41 +0200

    Add files via upload

---
+++ 20260428/Antony/Luchadores/src/luchadores/FinestraPrincipal.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luchadores;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author marc
+ */
+public class FinestraPrincipal extends JFrame{
+    
+    
+    public FinestraPrincipal() throws HeadlessException{
+    
+        initComponentsTitle();
+    
+    initContainers();
+    
+    }
+
+    private void initComponentsTitle() {
+
+        this.setTitle("Gestio Luchadors");
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setSize(1000, 600);
+        this.setLocationRelativeTo(null);
+          
+
+    }
+
+    private void initContainers() {
+
+        
+        Container principal = this.getContentPane();
+        
+        principal.setLayout(new GridLayout(1,2));
+        
+        PanelDatosEntrada datos = new PanelDatosEntrada();
+
+        
+        
+        principal.add(datos,BorderLayout.CENTER);
+        
+        PanelBotonesEntrada botones = new PanelBotonesEntrada();
+        
+        botones.setPreferredSize(new Dimension(0,150));
+        principal.add(botones);
+        
+        
+     }
+
+}
