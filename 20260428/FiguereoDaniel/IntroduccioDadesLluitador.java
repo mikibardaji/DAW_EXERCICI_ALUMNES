aadadd00063dd75694257393995cc3738fb6f6f7
44danif <danielf.m3007@gmail.com>
2026-05-04 20:16:31 +0200

    20280428

---
+++ 20260428/FiguereoDaniel/IntroduccioDadesLluitador.java
@@ -0,0 +1,16 @@
+package introducciodadeslluitador;
+
+public class IntroduccioDadesLluitador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        FinestraPrincipal app = new FinestraPrincipal();
+
+        app.setVisible(true);
+    }
+
+}
