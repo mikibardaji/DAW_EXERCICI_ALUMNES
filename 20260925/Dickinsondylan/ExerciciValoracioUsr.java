140639748d7ab55631827855c22d56951b3da1db
Dylan <mythsxndlies@gmail.com>
2026-09-25 13:35:17 +0200

    20260925

--- 20260925/Dickinsondylan/ExerciciValoracioUsr.java
+++ 20260925/Dickinsondylan/ExerciciValoracioUsr.java
@@ -10,27 +10,27 @@ import java.util.Scanner;
  * @author cdi1866
  */
 public class ExerciciValoracioUser {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //1.Inicio
     //2.Declaración variables y Scanner
-    double valUser1, valUser2, valUser3, valTotal;
+        int valUser1, valUser2, valUser3;
+        double valTotal;
             Scanner sc = new Scanner(System.in);
     // 3.Mostrar “Que valoracion te ha puesto el primer usuario?”
         System.out.print("Que valoracion te ha puesto el primer usuario?  ");
     //4.Esperar valUser1
-        valUser1 = sc.nextDouble();
+        valUser1 = sc.nextInt();
     // 5.Mostrar “Que valoracion te ha puesto el segundo usuario?”
          System.out.print("Que valoracion te ha puesto el segundo usuario?  ");
          //6.Esperar valUser2
-         valUser2= sc.nextDouble();
+         valUser2= sc.nextInt();
     //7.Mostrar “Que valoracion te ha puesto el tercer ususario?
         System.out.print("Que valoracion te ha puesto el tercer usuario?  ");
     //8.Esperar valUser3
-        valUser3= sc.nextDouble();
+        valUser3= sc.nextInt();
     //9. Calcular valTotal =(valUser1 + valUser2 + valUser3) / 3
         valTotal =(valUser1 + valUser2 + valUser3) / 3;
     //10. Mostrar “Tu valoración media es de  “ + valTotal :)
