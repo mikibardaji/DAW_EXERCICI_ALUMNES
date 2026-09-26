89738af75aa3368ad2c7e1cb3a71ddbc0eab70d7
adaamek6 <adaamek6@gmail.com>
2026-09-26 17:07:17 +0200

    20260929

---
+++ 20260929/ErrizqiKhnatiAdam/Ex3ProvenShare.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex3provenshare;
+
+/**
+ *
+ * @author aer5219
+ */
+public class Ex3ProvenShare {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Mostrar "Quants crèdits actuals tens?"
+        // Esperar creditsActuals
+        // Mostrar "Quin es el preu en crèdits per hora del servei contractat?"
+        // Esperar preuServei
+        // Mostrar "De quantes hores es el servei?"·
+        // Esperar horesServei
+        // Calcular costServei = preuServei * horesServei
+        // Comprovar comprovacio = creditsActuals > costServei
+        // Mostrar "Pots comprar-ho?: " + comprovacio
+    }
+   
+}
